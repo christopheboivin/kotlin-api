0f7b3aaa5398f49f7b25743794e303da6df10c0e
Christophe Boivin <christophe.boivin@gmail.com>
2019-04-12 21:00:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kotlin REST API test
+
+## How to build
+`mvn clean package`
+## TODO
+- Start script
---
+++ src/main/kotlin/fr/cbo/test/kotlin/KotlinApiTestApplication.kt
@@ -0,0 +1,11 @@
+package fr.cbo.test.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApiTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinApiTestApplication>(*args)
+}
